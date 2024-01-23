f01ef892ec414518eb05fe4b825a75ede484f7d8
renzo <desarrollo.jadalsoftware@gmail.com>
2024-01-23 16:54:39 -0500

    fix:base / deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dashtic-ts/preview/', 
+  // base: '/dashtic-ts/preview/', 
+  base: '/', 
   plugins: [react()],
   define: {
     'process.env': {}

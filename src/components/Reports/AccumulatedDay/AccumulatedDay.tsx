7820c0b5892e79bb17e2eb09165dbc12d498ce33
renzo <desarrollo.jadalsoftware@gmail.com>
2024-02-13 12:24:34 -0500

    fix:report accumulated day

--- src/components/Reports/AccumulatedDay/AccumulatedDay.tsx
+++ src/components/Reports/AccumulatedDay/AccumulatedDay.tsx
@@ -34,10 +34,9 @@ const AccumulatedDay = () => {
 
   const [pdfUrl, setPdfUrl] = useState<string | null>(null);
 
-  const [reportData, setReportData] = useState<any[]>([]); // Estado para almacenar los datos
+  const [reportData, setReportData] = useState<any[]>([]);
   const openTable = async () => {
     try {
-      // Convertir las fechas a formato legible antes de enviarlas
       const formattedStartDate =
         startDate?.day + "-" + startDate?.month.number + "-" + startDate?.year;
       const formattedEndDate =
@@ -54,14 +53,13 @@ const AccumulatedDay = () => {
         console.error("Las fechas no son válidas");
         return;
       }
-
-      // Enviar las fechas formateadas al endpoint
+      
       const response = await report.post("api/report/accumulated/day/table", {
         startDate: formattedStartDate,
         endDate: formattedEndDate,
       });
 
-      console.log(response.data); // Imprimir los datos en la consola
+      console.log(response.data); 
       setReportData(response.data);
     } catch (error) {
       console.error("Error al cargar los datos del informe", error);
@@ -70,67 +68,62 @@ const AccumulatedDay = () => {
 
   const openPdf = async () => {
     try {
-      // Convertir las fechas a formato legible antes de enviarlas
       const formattedStartDate =
         startDate?.day + "-" + startDate?.month.number + "-" + startDate?.year;
       const formattedEndDate =
         endDate?.day + "-" + endDate?.month.number + "-" + endDate?.year;
-  
+
       console.log(
         "formattedStartDate",
         formattedStartDate,
         "formattedEndDate",
         formattedEndDate
       );
-  
+
       if (!formattedStartDate || !formattedEndDate) {
         console.error("Las fechas no son válidas");
         return;
       }
-  
-      // Enviar las fechas formateadas al endpoint
-      const response = await report.post("api/report/accumulated/day/pdf", {
-        startDate: formattedStartDate,
-        endDate: formattedEndDate,
-      }, {
-        responseType: 'blob' // Especificar que esperamos una respuesta de tipo blob
-      });
-  
-      // Convertir la respuesta a un ArrayBuffer
+
+      const response = await report.post(
+        "api/report/accumulated/day/pdf",
+        {
+          startDate: formattedStartDate,
+          endDate: formattedEndDate,
+        },
+        {
+          responseType: "blob",
+        }
+      );
+
       const arrayBuffer = await response.data.arrayBuffer();
-  
-      // Crear un objeto Blob con los datos del PDF
+
       const blob = new Blob([arrayBuffer], { type: "application/pdf" });
       const url = URL.createObjectURL(blob);
-  
-      // Abrir el PDF en una nueva ventana
+
       window.open(url, "_blank");
     } catch (error) {
       console.error("Error al cargar los datos del informe", error);
     }
   };
-  
-  
 
   const openExcel = async () => {
     try {
-      // Convertir las fechas a formato legible antes de enviarlas
       const formattedStartDate = `${startDate?.day}-${startDate?.month.number}-${startDate?.year}`;
       const formattedEndDate = `${endDate?.day}-${endDate?.month.number}-${endDate?.year}`;
-  
+
       console.log(
         "formattedStartDate",
         formattedStartDate,
         "formattedEndDate",
         formattedEndDate
       );
-  
+
       if (!formattedStartDate || !formattedEndDate) {
         console.error("Las fechas no son válidas");
         return;
       }
-  
-      // Enviar las fechas formateadas al endpoint
+
       const response = await report.post(
         "api/report/accumulated/day/excel",
         {
@@ -141,23 +134,19 @@ const AccumulatedDay = () => {
           responseType: "blob", // Especificar que esperamos una respuesta de tipo blob
         }
       );
-  
-      // Convertir la respuesta a un ArrayBuffer
+
       const arrayBuffer = await response.data.arrayBuffer();
-  
-      // Crear un objeto Blob con los datos del Excel
-      const blob = new Blob([arrayBuffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+
+      const blob = new Blob([arrayBuffer], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
       const url = URL.createObjectURL(blob);
-  
-      // Abrir el Excel en una nueva ventana
+
       window.open(url, "_blank");
     } catch (error) {
       console.error("Error al cargar los datos del informe", error);
     }
   };
-  
-
-  
 
   const handleDateChange = (date) => {
     const formattedDate = format(date, "yyyy-MM-dd");
@@ -271,86 +260,83 @@ const AccumulatedDay = () => {
                   <div className="mb-3 mt-3">
                     <Label className="form-label">Opciones:</Label>
                   </div>
-                  
-                  <div className="wd-200 mg-b-30 mb-3 mt-3 d-flex justify-content-between">
-                  <Row>
-                  <Col lg="12">
-                    <Button
-                      color=""
-                      type="button"
-                      className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4"
-                      onClick={() => openTable()}
-                    >
-                      <svg
-                        className="svg-icon"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        width="24"
-                      >
-                        <path d="M0 0h24v24H0V0z" fill="none"></path>
-                        <path
-                          d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
-                          opacity=".3"
-                        ></path>
-                        <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
-                      </svg>
-                      <span className="btn-svg-text">FILTRAR</span>
-                    </Button>
-                    </Col>
-                    </Row>
 
+                  <div className="wd-200 mg-b-30 mb-3 mt-3 d-flex justify-content-between">
+                    <Row>
+                      <Col lg="12">
+                        <Button
+                          color=""
+                          type="button"
+                          className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4"
+                          onClick={() => openTable()}
+                        >
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path
+                              d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
+                              opacity=".3"
+                            ></path>
+                            <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+                          </svg>
+                          <span className="btn-svg-text">FILTRAR</span>
+                        </Button>
+                      </Col>
+                    </Row>
 
                     <Row>
-                    <Col lg="12">
-                    <Button
-                      color=""
-                      type="button"
-                      className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4 "
-                      onClick={() => openPdf()}
-                    >
-                      <svg
-                        className="svg-icon"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        width="24"
-                      >
-                        <path d="M0 0h24v24H0V0z" fill="none"></path>
-                        <path
-                          d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
-                          opacity=".3"
-                        ></path>
-                        <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
-                      </svg>
-                      <span className="btn-svg-text">PDF</span>
-                    </Button>
-                    <Button
-                      color=""
-                      type="button"
-                      className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4 "
-                      onClick={() => openExcel()}
-                    >
-                      <svg
-                        className="svg-icon"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        width="24"
-                      >
-                        <path d="M0 0h24v24H0V0z" fill="none"></path>
-                        <path
-                          d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
-                          opacity=".3"
-                        ></path>
-                        <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
-                      </svg>
-                      <span className="btn-svg-text">EXCEL</span>
-                    </Button>      
-                    </Col>
+                      <Col lg="12">
+                        <Button
+                          color=""
+                          type="button"
+                          className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4 "
+                          onClick={() => openPdf()}
+                        >
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path
+                              d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
+                              opacity=".3"
+                            ></path>
+                            <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+                          </svg>
+                          <span className="btn-svg-text">PDF</span>
+                        </Button>
+                        <Button
+                          color=""
+                          type="button"
+                          className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4 "
+                          onClick={() => openExcel()}
+                        >
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path
+                              d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
+                              opacity=".3"
+                            ></path>
+                            <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+                          </svg>
+                          <span className="btn-svg-text">EXCEL</span>
+                        </Button>
+                      </Col>
                     </Row>
-                    
-
                   </div>
                 </Col>
 

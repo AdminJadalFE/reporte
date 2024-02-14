c3620435b61da3ee819f09d711e7d258501e473a
renzo <desarrollo.jadalsoftware@gmail.com>
2024-02-14 15:38:10 -0500

    collection cash profitability

---
+++ src/components/Reports/Collection/DataTable/Basictable.tsx
@@ -0,0 +1,220 @@
+import React from "react";
+import { Button, Input } from "reactstrap";
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  usePagination,
+} from "react-table";
+
+export const BasicTable = ({ data }: any) => {
+  console.log('basicTable data', data);
+  const columns: any = React.useMemo(
+    () => [
+      {
+        Header: "Fecha",
+        accessor: "fecha",
+      },
+      {
+        Header: "Razón Social",
+        accessor: "84 OCT(produc)-Galones",
+      },
+      {
+        Header: "Consumo Cliente",
+        accessor: "84 OCT(produc)-Soles",
+      },
+      {
+        Header: "Pagos a Cuenta",
+        accessor: "90 OCT-Galones(produc)",
+      },      
+      {
+        Header: "Saldo Deudor",
+        accessor: "90 OCT-Galones(product)",
+      },                                         
+    ],
+    []
+  );
+
+  const tableInstance: any = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageSize: 10 }, // Puedes ajustar el número de filas por página aquí
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const {
+    getTableProps,
+    headerGroups,
+    getTableBodyProps,
+    prepareRow,
+    state,
+    setGlobalFilter,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    setPageSize,
+  } = tableInstance;
+
+  const { globalFilter, pageIndex, pageSize } = state;
+
+  return (
+    <>
+      <div className="d-flex">
+        <select
+          className=" mb-4 selectpage border me-1"
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      </div>
+      <div className="table-responsive">
+        <table
+          {...getTableProps()}
+          className="table table-bordered table-hover mb-0 table-striped card-table table-vcenter text-nowrap mb-0 table-bordered border-top-0"
+          style={{ fontSize: "10px" }} // Reduce el tamaño de la fuente de la tabla
+        >
+          <thead>
+            {headerGroups.map((headerGroup: any) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column: any) => (
+                  <th
+                    className="bg-primary text-white"
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                    // className={column.className}
+                    // style={{ padding: "1px", fontSize: "9px"}}
+                  >
+                    <span className="tabletitle">
+                      {column.render("Header")}
+                    </span>
+                    <span className="d-flex ms-auto">
+                      {column.isSorted ? (
+                        column.isSortedDesc ? (
+                          <i className="fa fa-angle-down"></i>
+                        ) : (
+                          <i className="fa fa-angle-up"></i>
+                        )
+                      ) : (
+                        ""
+                      )}
+                    </span>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row: any) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell: any) => {
+                    return (
+                      <td className="borderrigth" {...cell.getCellProps()} style={{ padding: "4px", fontSize: "12px" }}> {/* Ajusta el tamaño de fuente y el padding de las celdas */}
+                        {cell.render("Cell")}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="d-block d-sm-flex mt-4 ">
+        <span className="">
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span className="ms-sm-auto ">
+          <Button
+            color=""
+            className="btn-default tablebutton me-2 d-sm-inline d-block my-1"
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+          >
+            {" Previous "}
+          </Button>
+          <Button
+            color=""
+            className="btn-default tablebutton me-2 my-1"
+            onClick={() => {
+              previousPage();
+            }}
+            disabled={!canPreviousPage}
+          >
+            {" << "}
+          </Button>
+          <Button
+            color=""
+            className="btn-default tablebutton me-2 my-1"
+            onClick={() => {
+              previousPage();
+            }}
+            disabled={!canPreviousPage}
+          >
+            {" < "}
+          </Button>
+          <Button
+            color=""
+            className="btn-default tablebutton me-2 my-1"
+            onClick={() => {
+              nextPage();
+            }}
+            disabled={!canNextPage}
+          >
+            {" > "}
+          </Button>
+          <Button
+            color=""
+            className="btn-default tablebutton me-2 my-1"
+            onClick={() => {
+              nextPage();
+            }}
+            disabled={!canNextPage}
+          >
+            {" >> "}
+          </Button>
+          <Button
+            color=""
+            className="btn-default tablebutton me-2 d-sm-inline d-block my-1"
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {" Next "}
+          </Button>
+        </span>
+      </div>
+    </>
+  );
+};
+
+const GlobalFilter = ({ filter, setFilter }: any) => {
+  return (
+    <span className="d-flex ms-auto">
+      <Input
+        value={filter || ""}
+        onChange={(e) => setFilter(e.target.value)}
+        className="form-control mb-4"
+        placeholder="Search..."
+        style={{ fontSize: "10px" }} // Reduce el tamaño de la fuente del filtro global
+      />
+    </span>
+  );
+};
--- src/components/Reports/PrettyCash/PrettyCash.tsx
+++ src/components/Reports/PrettyCash/PrettyCash.tsx
@@ -1,9 +1,294 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Col,
+  Row,
+  Label,
+  Button,
+} from "reactstrap";
+import { PageHeaders } from "../../../Shared/Prism/Prism";
+import DatePicker from "react-multi-date-picker";
+import Select from "react-select";
+import axios from "axios";
+import { report } from "../../../Util/axios";
+import { BasicTable } from "./DataTable/Basictable";
 
-export default function PrettyCash() {
+const PrettyCash = () => {
+  const [dates, setDates] = useState<any>();
+  const [countryOption, setCountryOption] = useState<any>(null);
+
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const Countryoptions = [
+    { value: "Principal", label: "Principal" },
+    { value: "Secundario", label: "Secundario" },
+    { value: "Tercer", label: "Tercer" },
+    { value: "Cuarto", label: "Cuarto" },
+  ];
+
+  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+
+  const [reportData, setReportData] = useState<any[]>([]);
+  const openTable = async () => {
+    try {
+      const formattedStartDate =
+        startDate?.day + "-" + startDate?.month.number + "-" + startDate?.year;
+      const formattedEndDate =
+        endDate?.day + "-" + endDate?.month.number + "-" + endDate?.year;
+
+      console.log(
+        "formattedStartDate",
+        formattedStartDate,
+        "formattedEndDate",
+        formattedEndDate
+      );
+
+      if (!formattedStartDate || !formattedEndDate) {
+        console.error("Las fechas no son válidas");
+        return;
+      }
+      
+      const response = await report.post("api/report/accumulated/day/table", {
+        startDate: formattedStartDate,
+        endDate: formattedEndDate,
+      });
+
+      console.log(response.data); 
+      setReportData(response.data);
+    } catch (error) {
+      console.error("Error al cargar los datos del informe", error);
+    }
+  };
+  const openPdf = async () => {
+    try {
+      const response = await report.get("api/report/sale/pdf/day", {
+        responseType: "arraybuffer",
+      });
+      const blob = new Blob([response.data], { type: "application/pdf" });
+      const pdfUrl = URL.createObjectURL(blob);
+      setPdfUrl(pdfUrl);
+    } catch (error) {
+      console.error("Error al cargar el PDF", error);
+    }
+  };
+
   return (
     <div>
-      <h1>PrettyCash</h1>
+      <PageHeaders
+        title="Reporte Caja Chica"
+        home="Home"
+        name="Pages"
+        fonticonsname="Empty"
+      />
+
+      <Row>
+        <Col md="12">
+          <Card>
+            <CardHeader>
+              <CardTitle>Imprimir desde las fechas</CardTitle>
+            </CardHeader>
+            <CardBody>
+              <Row>
+                <Col lg="6">
+                  <div className="mb-3">
+                    <Label className="form-label">Desde: </Label>
+                  </div>
+                  <div className="wd-200 mg-b-30">
+                    <div className="input-group">
+                      <div className="input-group-text">
+                        <div className="">
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            width="18"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 2v3H4V5h16zM4 21V10h16v11H4z" />
+                            <path d="M4 5.01h16V8H4z" opacity=".3" />
+                          </svg>
+                        </div>
+                      </div>
+                      <DatePicker
+                        format="DD/MM/YYYY"
+                        className="form-control"
+                        placeholder="Desde"
+                        selected={startDate}
+                        onChange={(startDate) => {
+                          console.log(startDate);
+                          setStartDate(startDate);
+                        }}
+                        numberOfMonths={1}
+                      />
+                    </div>
+                  </div>
+                </Col>
+                <Col lg="6">
+                  <div className="mb-3">
+                    <Label className="form-label">Hasta: </Label>
+                  </div>
+                  <div className="wd-200 mg-b-30">
+                    <div className="input-group">
+                      <div className="input-group-text">
+                        <div className="">
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            width="18"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none" />
+                            <path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 2v3H4V5h16zM4 21V10h16v11H4z" />
+                            <path d="M4 5.01h16V8H4z" opacity=".3" />
+                          </svg>
+                        </div>
+                      </div>
+                      <DatePicker
+                        format="DD/MM/YYYY"
+                        className="form-control"
+                        placeholder="Hasta"
+                        value={endDate}
+                        onChange={(endDate) => setEndDate(endDate)}
+                        numberOfMonths={1}
+                      />
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col lg="6">
+                  <div className="mb-3 mt-3">
+                    <Label className="form-label">Selecciona un Local: </Label>
+                  </div>
+                  <div className="wd-200 mg-b-30">
+                    <Select
+                      defaultValue={countryOption}
+                      onChange={setCountryOption}
+                      options={Countryoptions}
+                      placeholder="Country"
+                      classNamePrefix="Search"
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col lg="12">
+                  <div className="mb-3 mt-3">
+                    <Label className="form-label">Opciones:</Label>
+                  </div>
+
+                  <div className="wd-200 mg-b-30 mb-3 mt-3 d-flex justify-content-between">
+                    <Row>
+                      <Col lg="12">
+                        <Button
+                          color=""
+                          type="button"
+                          className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4"
+                          onClick={() => openTable()}
+                        >
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path
+                              d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
+                              opacity=".3"
+                            ></path>
+                            <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+                          </svg>
+                          <span className="btn-svg-text">FILTRAR</span>
+                        </Button>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col lg="12">
+                        <Button
+                          color=""
+                          type="button"
+                          className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4 "
+                          onClick={() => openPdf()}
+                        >
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path
+                              d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
+                              opacity=".3"
+                            ></path>
+                            <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+                          </svg>
+                          <span className="btn-svg-text">PDF</span>
+                        </Button>
+                        <Button
+                          color=""
+                          type="button"
+                          className="btn btn-primary btn-svgs btn-svg-white mt-4 ml-4 mr-4 "
+                          onClick={() => openExcel()}
+                        >
+                          <svg
+                            className="svg-icon"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                          >
+                            <path d="M0 0h24v24H0V0z" fill="none"></path>
+                            <path
+                              d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z"
+                              opacity=".3"
+                            ></path>
+                            <path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+                          </svg>
+                          <span className="btn-svg-text">EXCEL</span>
+                        </Button>
+                      </Col>
+                    </Row>
+                  </div>
+                </Col>
+
+                {pdfUrl && (
+                  <div className="pdf-viewer">
+                    <object
+                      data={pdfUrl}
+                      type="application/pdf"
+                      width="100%"
+                      height="600px"
+                    >
+                      <p>
+                        Tu navegador no puede mostrar el PDF. Puedes descargarlo
+                        <a href={pdfUrl}>aquí</a>.
+                      </p>
+                    </object>
+                  </div>
+                )}
+              </Row>
+              <BasicTable data={reportData} />
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
+
+PrettyCash.propTypes = {};
+
+PrettyCash.defaultProps = {};
+
+export default PrettyCash;
